82a6ee3ca37a06df8c9b99b36e51fc5fe8feb8f7
Steven Riggs <steven.riggs@icloud.com>
2019-12-11 10:23:54 -0500

    add table with dynamic data

--- src/components/records/RecordsTable.js
+++ src/components/records/RecordsTable.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 function RecordsTable() {
-  return (
-    <div className='d-flex flex-row flex-grow-1'>
-        Content Middle
-    </div>
-  );
+    const items = [];
+    for (let i = 0; i < 10; i++) {
+        let theAItem = 'Data A-' + i;
+        let theBItem = 'Data B-' + i;
+        let theCItem = 'Data C-' + i;
+        let theDItem = 'Data D-' + i;
+        items.push({
+            dataA: theAItem,
+            dataB: theBItem,
+            dataC: theCItem,
+            dataD: theDItem
+        })
+    }
+    return (
+        <div className='d-flex flex-row flex-grow-1'>
+            <div className='d-flex flex-grow-1 overflow-auto'>
+                <Table striped bordered hover size="sm">
+                    <thead className='sticky-top bg-light'>
+                        <tr>
+                            <th>Header A</th>
+                            <th>Header B</th>
+                            <th>Header C</th>
+                            <th>Header D</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map(item=>
+                            <tr>
+                                <td>{item.dataA}</td>
+                                <td>{item.dataB}</td>
+                                <td>{item.dataC}</td>
+                                <td>{item.dataD}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
+        </div>
+    );
 }
 
 export default RecordsTable;
