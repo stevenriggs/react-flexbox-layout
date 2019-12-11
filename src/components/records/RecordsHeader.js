4e66d16aabf83d8fb60255a70f9f4ef40c6c8426
Steven Riggs <steven.riggs@icloud.com>
2019-12-11 13:22:39 -0500

    header padding

--- src/components/records/RecordsHeader.js
+++ src/components/records/RecordsHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RecordsHeader() {
   return (
-    <div className='d-flex flex-row flex-grow-1 justify-content-between'>
+    <div className='d-flex flex-row flex-grow-1 justify-content-between p-2'>
         <div className='d-flex'>
             Records Title
         </div>
