7b7b07d46a8306e3dbda7d40e9e975733bc94b53
Steven Riggs <steven.riggs@icloud.com>
2019-12-11 13:39:28 -0500

    some responsive

--- src/components/records/RecordsFooter.js
+++ src/components/records/RecordsFooter.js
@@ -4,7 +4,7 @@ import ItemsPerPageControl from './ItemsPerPageControl';
 
 function RecordsFooter() {
   return (
-    <div className='d-flex flex-row flex-grow-1 justify-content-between align-items-center bg-light border p-2'>
+    <div className='d-flex flex-row flex-grow-1 flex-wrap justify-content-between align-items-center bg-light border p-2'>
         <div className='d-flex'>
             <ItemsPerPageControl />
         </div>
