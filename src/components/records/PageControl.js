b5c075e4a47fdb46a2561febc7a455c79dc6434f
Steven Riggs <steven.riggs@icloud.com>
2019-12-11 13:55:30 -0500

    less page buttons for small screens

--- src/components/records/PageControl.js
+++ src/components/records/PageControl.js
@@ -9,12 +9,10 @@ function PageControl() {
             <Pagination.Prev />
             <Pagination.Item>{1}</Pagination.Item>
             <Pagination.Ellipsis />
-
-            <Pagination.Item>{10}</Pagination.Item>
+            
             <Pagination.Item>{11}</Pagination.Item>
             <Pagination.Item active>{12}</Pagination.Item>
             <Pagination.Item>{13}</Pagination.Item>
-            <Pagination.Item disabled>{14}</Pagination.Item>
 
             <Pagination.Ellipsis />
             <Pagination.Item>{20}</Pagination.Item>
