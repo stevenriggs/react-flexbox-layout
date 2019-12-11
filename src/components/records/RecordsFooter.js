2fb87d332ceb886a905b6d3d3a4b2ab97216cd38
Steven Riggs <steven.riggs@icloud.com>
2019-12-11 13:18:58 -0500

    better spacing

--- src/components/records/RecordsFooter.js
+++ src/components/records/RecordsFooter.js
@@ -3,7 +3,7 @@ import PageControl from './PageControl';
 
 function RecordsFooter() {
   return (
-    <div className='d-flex flex-row flex-grow-1 justify-content-between bg-light border pt-2'>
+    <div className='d-flex flex-row flex-grow-1 justify-content-between bg-light border pt-2 pr-2 pl-2'>
         <div className='d-flex'>
             Items Per Page Control
         </div>
